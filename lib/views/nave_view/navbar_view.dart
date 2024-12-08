3ff4440aef41ca4101f974e50011aa01552772de
sadiq awan <awansadiq09@gmail.com>
2024-12-08 20:51:21 +0500

    Initial commit

---
+++ lib/views/nave_view/navbar_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class NavbarView extends StatefulWidget {
+
+  const NavbarView({super.key});
+
+  @override
+  State<NavbarView> createState() => _NavbarViewState();
+}
+
+class _NavbarViewState extends State<NavbarView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
